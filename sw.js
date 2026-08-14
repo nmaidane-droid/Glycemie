640170229c49c9bedb50f415a9182b561785d6fa
nmaidane-droid <nmaidane@hotmail.com>
2026-08-14 20:09:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'glyca-v3';
+const FICHIERS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FICHIERS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(k => Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cache => {
+      const reseau = fetch(e.request).then(rep => {
+        if (rep && rep.status === 200 && rep.type !== 'opaque') {
+          const copie = rep.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copie));
+        }
+        return rep;
+      }).catch(() => cache);
+      return cache || reseau;
+    })
+  );
+});
